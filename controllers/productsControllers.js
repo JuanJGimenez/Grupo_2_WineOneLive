76b15834c90c0e08ca8fb3ad9316dad56f4d272b
unknown <angelicola.sebastian@gmail.com>
2022-06-20 16:40:17 -0300

    correciones

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -7,36 +7,89 @@ let productsControllers = {
             .then(function (product) {
                 res.render("products/productsList", { product })
             })
-        },
+    },
 
-    create: function(req, res) {
-            res.render("products/productsCreate")
-            },
+    create: function (req, res) {
+        res.render("products/productsCreate")
+    },
 
     detalleProducto: function (req, res) {
 
-       let recomendado = []
-       
-       db.Products.findAll({
-        where: {
-            recommended: 1
-        }
-    })
-        .then(function (product) {
-            recomendado = product
+        let recomendado = []
+
+        db.Products.findAll({
+            where: {
+                recommended: 1
+            }
         })
-           
+            .then(function (product) {
+                recomendado = product
+            })
+
 
         db.Products.findByPk(req.params.id)
             .then(Product => {
                 console.log(recomendado)
-                res.render("./products/product-detail", {Product: Product, recommended: recomendado });
+                res.render("./products/product-detail", { Product: Product, recommended: recomendado });
             });
-               
-                
-            
-           },
-      
+
+
+
+    },
+    
+    vinos: function (req, res) {
+
+
+        db.Products.findAll({
+            where: {
+                category_id: 1
+            }
+        })
+            .then(function (product) {
+                res.render("./products/vinos", { product });
+            })
+
+    },
+    cervezas: function (req, res) {
+
+
+        db.Products.findAll({
+            where: {
+                category_id: 2
+            }
+        })
+            .then(function (product) {
+                res.render("./products/cervezas", { product });
+            })
+
+    },
+    whiskys: function (req, res) {
+
+
+        db.Products.findAll({
+            where: {
+                category_id: 3
+            }
+        })
+            .then(function (product) {
+                res.render("./products/whiskys", { product });
+            })
+
+    },
+    espumantes: function (req, res) {
+
+
+        db.Products.findAll({
+            where: {
+                category_id: 4
+            }
+        })
+            .then(function (product) {
+                res.render("./products/espumantes", { product });
+            })
+
+    },
+
 }
 
 module.exports = productsControllers;
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 const productsControllers = require("../controllers/productsControllers.js");
 
-router.get('/list', productsControllers.list);
+router.get('/list/', productsControllers.list);
 router.get("/create", productsControllers.create);
 router.get("/product-detail/:id", productsControllers.detalleProducto);
+router.get("/list/vinos", productsControllers.vinos);
+router.get("/list/cervezas", productsControllers.cervezas);
+router.get("/list/whiskys", productsControllers.whiskys);
+router.get("/list/espumantes", productsControllers.espumantes);
 
 module.exports = router;
