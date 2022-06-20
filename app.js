73ec0395fe8c1c983294113422c491ea1db131d6
JuanJGimenez <mdqgimenez@gmail.com>
2022-06-20 13:47:37 -0300

    app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/productsList", productsRouter);
+app.use("/products", productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
