0b10350d98c21b5b7257349d5ba183aacd8e728a
unknown <angelicola.sebastian@gmail.com>
2022-06-22 02:29:08 -0300

    agregando algunas validaciones del front de prueba

---
+++ public/js/formValidation.js
@@ -0,0 +1,64 @@
+window.onload = function(){
+
+
+
+
+    // inputs capturados
+    let inputName = document.getElementById('product_name');
+    let inputPrice = document.getElementById('price');
+    let inputDescription = document.getElementById('product_description');
+   
+
+    // form capturado
+    let form = document.querySelector('.form');
+
+    let ulErrores = document.querySelector('.errores');
+
+    inputName.focus();
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let errores = [];
+
+        if(inputName.value === ""){
+            inputName.classList.add('is-invalid');
+            errores.push("Debes introducir un título");
+        } else {
+            inputName.classList.remove('is-invalid');
+            inputName.classList.add('is-valid');
+        }
+
+        if(inputPrice.value === ""){
+            inputPrice.classList.add('is-invalid');
+            errores.push("Debes introducir un precio");
+        } else if(inputPrice.value < 1 ){
+            inputPrice.classList.add('is-invalid');
+            errores.push("el precio debe ser mayor a 0");
+            
+        } 
+        
+        else {
+            inputPrice.classList.remove('is-invalid');
+            inputPrice.classList.add('is-valid');
+        }        
+        
+        if(inputDescription.value === ""){
+            inputDescription.classList.add('is-invalid');
+            errores.push("Debes introducir una descripción");
+        } else {
+            inputDescription.classList.remove('is-invalid');
+            inputDescription.classList.add('is-valid');
+        }        
+        
+        ulErrores.innerHTML = '';
+        if(errores.length > 0){
+            ulErrores.classList.add('alert-warning');
+            for(let i = 0; i < errores.length; i++){
+                ulErrores.innerHTML += '<ol>' + errores[i]+ '</ol>'+" "
+            }
+        } else {
+            form.submit()
+        }
+    })
+}
---
+++ public/js/userValidation.js
@@ -0,0 +1,74 @@
+window.onload = function(){
+
+
+
+
+    // inputs capturados
+    let inputName = document.getElementById('user_first_name');
+    let inputSurname = document.getElementById('user_last_name');
+    let inputEmail = document.getElementById('user_email');
+    let inputPassword = document.getElementById('user_password');
+   
+
+    // form capturado
+    let form = document.querySelector('.form');
+
+    let ulErrores = document.querySelector('.errores');
+
+    inputName.focus();
+
+   
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let errores = [];
+
+        if(inputName.value === ""){
+            inputName.classList.add('is-invalid');
+            errores.push("Debes introducir un nombre");
+        } else {
+            inputName.classList.remove('is-invalid');
+            inputName.classList.add('is-valid');
+        }
+
+        if(inputSurname.value === ""){
+            inputSurname.classList.add('is-invalid');
+            errores.push("Debes introducir un apellido");
+        }
+        else {
+            inputSurname.classList.remove('is-invalid');
+            inputSurname.classList.add('is-valid');
+        }        
+        
+        if(inputEmail.value === ""){
+            inputEmail.classList.add('is-invalid');
+            errores.push("Debes introducir un Email");
+        } 
+        
+        
+        else {
+            inputEmail.classList.remove('is-invalid');
+            inputEmail.classList.add('is-valid');
+
+        }        
+
+        if(inputPassword.value === ""){
+            inputPassword.classList.add('is-invalid');
+            errores.push("Debes introducir una contraseña");
+        } else {
+            inputPassword.classList.remove('is-invalid');
+            inputPassword.classList.add('is-valid');
+        }
+        
+        ulErrores.innerHTML = '';
+        if(errores.length > 0){
+            ulErrores.classList.add('alert-warning');
+            for(let i = 0; i < errores.length; i++){
+                ulErrores.innerHTML += '<ol>' + errores[i]+ '</ol>'+" "
+            }
+        } else {
+            form.submit()
+        }
+    })
+}
